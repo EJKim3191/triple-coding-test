16c8c2b2d777c10a3a6545412c4c2bf650eac80e
EJKim <dmdwn3979@gmail.com>
2022-06-16 19:55:32 +0900

    Add: AwardContainer molecule

---
+++ src/components/molecules/AwardContainer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as Styled from './style';
+
+interface Props {
+
+}
+
+function AwardContainer({
+
+}: Props): React.ReactElement {
+
+
+    return (
+        <Styled.AwardContainer>
+
+        </Styled.AwardContainer>
+    );
+}
+
+export default AwardContainer;
