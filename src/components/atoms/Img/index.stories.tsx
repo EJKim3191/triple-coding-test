e5ac0cfae486488f5f1fdac64277bf31298fab06
EJKim <dmdwn3979@gmail.com>
2022-06-16 22:38:35 +0900

    Add: storybook 추가

---
+++ src/components/atoms/Img/index.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Img from '.';
+import badgeApple4x from '../../../assets/img/badge-apple4x.png';
+import triple2x from '../../../assets/img/triple2x.png';
+
+export default {
+    title: 'Atoms / Img',
+};
+
+export const BadgeApple4x: React.FC = () => (
+    <div style={{ padding: '1rem', backgroundColor: 'gray' }}>
+        <div style={{ width: '100%' }}>
+            <Img
+                alt={'1'}
+                src={badgeApple4x}
+            />
+        </div>
+    </div>
+
+);
+
+export const Triple2x: React.FC = () => (
+    <div style={{ padding: '1rem', backgroundColor: 'gray' }}>
+        <div style={{ width: '100%' }}>
+            <Img
+                alt={'2'}
+                src={triple2x}
+            />
+        </div>
+    </div>
+)
+
