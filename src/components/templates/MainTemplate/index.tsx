d1f991c24b941a4b8293352a1b83830aa250e22e
EJKim <dmdwn3979@gmail.com>
2022-06-16 19:56:17 +0900

    Add: MainPage template

---
+++ src/components/templates/MainTemplate/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as Styled from './style'
+const MainTemplate: React.FC = () => {
+	return (
+		<Styled.MainTemplate >
+            아토믹 패턴 템플릿 입니다
+		</Styled.MainTemplate>
+	)
+}
+
+export default MainTemplate;
