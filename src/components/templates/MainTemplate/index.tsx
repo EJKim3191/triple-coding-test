dcf71e9f24bc54a4203fdf9de6b35a3f47aa7689
EJKim <dmdwn3979@gmail.com>
2022-06-18 22:23:11 +0900

    Add: 요구사항 외 구현

--- src/components/templates/MainTemplate/index.tsx
+++ src/components/templates/MainTemplate/index.tsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import * as Styled from './style'
+import StatisticSection from '../../organisms/StatisticSection'
+/**
+ * 구현 해야하는 내용 외의 내용은 간단하게 표현하였습니다 (hardCoding)
+ * 
+ */
+import bgImg from '../../../assets/img/img-01-bg.png';
+const commonStyle = {
+	height: '1000px',
+	position: 'relative',
+	overflow: 'hidden',
+	backgroundImage: 'initial',
+	backgroundPositionX: 'center',
+	backgroundPositionY: 'center',
+	backgroundSize: 'cover',
+	backgroundRepeatX: 'initial',
+	backgroundRepeatY: 'initial',
+	backgroundAttachment: 'initial',
+	backgroundOrigin: 'initial',
+	backgroundClip: 'initial',
+} as const;
+
+const styles = {
+	gray_bg_1: {
+		...commonStyle,
+		background: 'center center / cover rgb(250, 250, 250)',
+		backgroundColor: 'rgb(250, 250, 250)'
+	},
+	gray_bg_2: {
+		...commonStyle,
+		background: 'center center / cover rgb(245, 245, 245)',
+		backgroundColor: 'rgb(245, 245, 245)'
+	}
+} as const;
+
 const MainTemplate: React.FC = () => {
 	return (
 		<Styled.MainTemplate >
-            아토믹 패턴 템플릿 입니다
+			<Styled.SectionContainer style={{height: '860px', backgroundImage: `url(${bgImg})`, backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%'}}>
+				1
+			</Styled.SectionContainer >
+            <Styled.SectionContainer>
+				<StatisticSection />
+			</Styled.SectionContainer>
+			<Styled.SectionContainer style={styles.gray_bg_1}>
+				2
+			</Styled.SectionContainer>
+			<Styled.SectionContainer style={styles.gray_bg_2}>
+				3
+			</Styled.SectionContainer>
+			<Styled.SectionContainer style={styles.gray_bg_1}>
+				4
+			</Styled.SectionContainer>
 		</Styled.MainTemplate>
 	)
 }
--- src/components/templates/MainTemplate/style.ts
+++ src/components/templates/MainTemplate/style.ts
@@ -3,3 +3,8 @@ import styled from 'styled-components';
 export const MainTemplate = styled.div`
 
 `;
+
+export const SectionContainer = styled.div`
+    position: relative;
+    overflow: hidden;
+`
