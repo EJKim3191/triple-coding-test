70f0b6e8793797c83723c082cc0e61d8f4617842
EJKim <dmdwn3979@gmail.com>
2022-06-18 22:25:34 +0900

    Fix: Img atom 수정

--- src/components/atoms/Img/index.stories.tsx
+++ src/components/atoms/Img/index.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Img from '.';
 import badgeApple4x from '../../../assets/img/badge-apple4x.png';
 import triple2x from '../../../assets/img/triple2x.png';
-
+import playstore2x from '../../../assets/img/play-store2x.png';
 export default {
     title: 'Atoms / Img',
 };
@@ -18,7 +18,16 @@ export const BadgeApple4x: React.FC = () => (
     </div>
 
 );
-
+export const PlayStore2x: React.FC = () => (
+    <div style={{ padding: '1rem', backgroundColor: 'gray' }}>
+        <div style={{ width: '100%' }}>
+            <Img
+                alt={'2'}
+                src={playstore2x}
+            />
+        </div>
+    </div>
+)
 export const Triple2x: React.FC = () => (
     <div style={{ padding: '1rem', backgroundColor: 'gray' }}>
         <div style={{ width: '100%' }}>
